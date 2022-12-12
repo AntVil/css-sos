b58f660dcd5eba870e26fdd490b32acc3e8e82f7
AntVil <antboehler@gmail.com>
2022-12-12 14:38:23 +0100

    more on button (info)

--- scripts/util.js
+++ scripts/util.js
@@ -4,6 +4,12 @@ function randInt(min, max){
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+function randFloat(min, max){
+    min = parseFloat(min);
+    max = parseFloat(max);
+    return Math.random() * (max - min) + min;
+}
+
 function randColor(){
     let val = "#" + Math.floor(Math.random() * 16777215).toString(16);
     while (val.length < 7) {
--- sos/button/index.js
+++ sos/button/index.js
@@ -9,13 +9,18 @@ let buttonHoverColorInput;
 let infoInput;
 let infoTriggerHoverInput;
 let infoTriggerClickedInput;
-let infoLocationInput;
+let infoLocationTopInput;
+let infoLocationRightInput;
+let infoLocationBottomInput;
+let infoLocationLeftInput;
 let infoTextInput;
 let infoTimespanInput;
 let infoWidthInput;
 let infoHeightInput;
 let infoArrowLengthInput;
 let infoArrowSizeInput;
+let infoColorInput;
+let infoFontColorInput;
 
 setup = () => {
     buttonWidthInput = document.getElementById("buttonWidth");
@@ -29,13 +34,18 @@ setup = () => {
     infoInput = document.getElementById("info");
     infoTriggerHoverInput = document.getElementById("infoTriggerHover");
     infoTriggerClickedInput = document.getElementById("infoTriggerClicked");
-    infoLocationInput = document.getElementById("infoLocation");
+    infoLocationTopInput = document.getElementById("infoLocationTop");
+    infoLocationRightInput = document.getElementById("infoLocationRight");
+    infoLocationBottomInput = document.getElementById("infoLocationBottom");
+    infoLocationLeftInput = document.getElementById("infoLocationLeft");
     infoTextInput = document.getElementById("infoText");
     infoTimespanInput = document.getElementById("infoTimespan");
     infoWidthInput = document.getElementById("infoWidth");
     infoHeightInput = document.getElementById("infoHeight");
     infoArrowLengthInput = document.getElementById("infoArrowLength");
     infoArrowSizeInput = document.getElementById("infoArrowSize");
+    infoColorInput = document.getElementById("infoColor");
+    infoFontColorInput = document.getElementById("infoFontColor");
 
     random();
 }
@@ -49,18 +59,102 @@ update = () => {
     let buttonBorderColor = buttonBorderColorInput.value;
     let buttonFontColor = buttonFontColorInput.value;
     let buttonHoverColor = buttonHoverColorInput.value;
-    let info = parseFloat(infoInput.value);
+    let info = infoInput.checked;
     let infoTriggerHover = infoTriggerHoverInput.checked;
     let infoTriggerClicked = infoTriggerClickedInput.checked;
-    let infoLocation = parseFloat(infoLocationInput.value);
+    let infoLocationTop = infoLocationTopInput.checked;
+    let infoLocationRight = infoLocationRightInput.checked;
+    let infoLocationBottom = infoLocationBottomInput.checked;
+    let infoLocationLeft = infoLocationLeftInput.checked;
     let infoText = infoTextInput.value;
     let infoTimespan = parseFloat(infoTimespanInput.value);
     let infoWidth = parseFloat(infoWidthInput.value);
     let infoHeight = parseFloat(infoHeightInput.value);
     let infoArrowLength = parseFloat(infoArrowLengthInput.value);
     let infoArrowSize = parseFloat(infoArrowSizeInput.value);
+    let infoColor = infoColorInput.value;
+    let infoFontColor = infoFontColorInput.value;
+
+    let infoTop;
+    let infoActiveTop
+    let infoActiveArrowTop
+    let infoLeft;
+    let infoActiveLeft;
+    let infoActiveArrowLeft;
+    let infoArrowWidth;
+    let infoArrowHeight;
+    let infoTransform;
+    let imgUrl;
+    if(infoLocationTop){
+        infoTop = "100%";
+        infoActiveTop = `${-(infoArrowLength + buttonBorderWidth)}px`;
+        infoActiveArrowTop = `${-(buttonBorderWidth + 1)}px`;
+        infoLeft = "50%";
+        infoActiveLeft = `50%`;
+        infoActiveArrowLeft = `50%`;
+        infoArrowWidth = `${infoArrowSize}px`;
+        infoArrowHeight = `${infoArrowLength}px`;
+        infoTransform = "translate(-50%, -100%)";
+        imgUrl = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${infoArrowSize} ${infoArrowLength}'%3E%3Cpath d='M0 0L${infoArrowSize} 0L${infoArrowSize / 2} ${infoArrowLength}'/%3E%3C/svg%3E")`;
+    }else if(infoLocationRight){
+        infoTop = "50%";
+        infoActiveTop = `50%`;
+        infoActiveArrowTop = `50%`;
+        infoLeft = "-100%";
+        infoActiveLeft = `calc(100% + ${infoArrowLength + buttonBorderWidth}px)`;
+        infoActiveArrowLeft = `calc(100% + ${buttonBorderWidth + 1}px)`;
+        infoArrowWidth = `${infoArrowLength}px`;
+        infoArrowHeight = `${infoArrowSize}px`;
+        infoTransform = "translate(0, -50%)";
+        imgUrl = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${infoArrowLength} ${infoArrowSize}'%3E%3Cpath d='M${infoArrowLength} 0L0 ${infoArrowSize / 2}L${infoArrowLength} ${infoArrowSize}'/%3E%3C/svg%3E")`;
+    }else if(infoLocationBottom){
+        infoTop = "-100%";
+        infoActiveTop = `calc(100% + ${infoArrowLength + buttonBorderWidth}px)`;
+        infoActiveArrowTop = `calc(100% + ${buttonBorderWidth + 1}px)`;
+        infoLeft = "50%";
+        infoActiveLeft = `50%`;
+        infoActiveArrowLeft = `50%`;
+        infoArrowWidth = `${infoArrowSize}px`;
+        infoArrowHeight = `${infoArrowLength}px`;
+        infoTransform = "translate(-50%, 0)";
+        imgUrl = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${infoArrowSize} ${infoArrowLength}'%3E%3Cpath d='M0 ${infoArrowLength}L${infoArrowSize} ${infoArrowLength}L${infoArrowSize / 2} 0'/%3E%3C/svg%3E")`;
+    }else if(infoLocationLeft){
+        infoTop = "50%";
+        infoActiveTop = `50%`;
+        infoActiveArrowTop = `50%`;
+        infoLeft = "100%";
+        infoActiveLeft = `${-(infoArrowLength + buttonBorderWidth)}px`;
+        infoActiveArrowLeft = `${-(buttonBorderWidth + 1)}px`;
+        infoArrowWidth = `${infoArrowLength}px`;
+        infoArrowHeight = `${infoArrowSize}px`;
+        infoTransform = "translate(-100%, -50%)";
+        imgUrl = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${infoArrowLength} ${infoArrowSize}'%3E%3Cpath d='M0 0L${infoArrowLength} ${infoArrowSize / 2}L0 ${infoArrowSize}'/%3E%3C/svg%3E")`;
+    }
     
-    if(infoTriggerHover){
+    if(!info){
+        return `
+            #output>button{
+                position: relative;
+                width: ${buttonWidth + 2 * buttonBorderWidth}px;
+                height: ${buttonHeight + 2 * buttonBorderWidth}px;
+                border: ${buttonBorderWidth}px solid ${buttonBorderColor};
+                border-radius: ${buttonBorderRadius}px;
+                background-color: ${buttonColor};
+                color: ${buttonFontColor};
+            }
+
+            #output>button:disabled{
+                filter: grayscale(100%);
+            }
+
+            @media (hover: hover) {
+                #output>button:not([disabled]):hover{
+                    background-color: ${buttonHoverColor};
+                    cursor: pointer;
+                }
+            }
+        `;
+    }else if(infoTriggerHover){
         return `
             #output>button{
                 position: relative;
@@ -76,56 +170,72 @@ update = () => {
                 content: "";
                 opacity: 0;
                 position: absolute;
-                top: 0;
-                width: ${infoArrowSize};
-                height: ${infoArrowLength};
-                transition: 0s 1s;
-
-                background-color: ${0};
-                -webkit-mask-image: ${0};
-                mask-image: ${0};
-                -webkit-mask-size: ${0}px;
-                mask-size: ${0}px;
+                left: ${infoLeft};
+                top: ${infoTop};
+                width: ${infoArrowWidth};
+                height: ${infoArrowHeight};
+                transform: ${infoTransform};
+                transition: 0s ${infoTimespan}s;
+                background-color: ${infoColor};
+                -webkit-mask-image: ${imgUrl};
+                mask-image: ${imgUrl};
+                -webkit-mask-size: ${infoArrowWidth} ${infoArrowHeight};
+                mask-size: ${infoArrowWidth} ${infoArrowHeight};
                 -webkit-mask-position: center center;
                 mask-position: center center;
                 -webkit-mask-repeat: no-repeat;
                 mask-repeat: no-repeat;
                 background-blend-mode: overlay;
+                z-index: 1;
             }
             
             #output>button:not([disabled]):active::before{
                 opacity: 1;
-                left: 50%;
-                transform: translate(-50%, -100%);
-                border-top: ${infoArrowLength + 1}px solid var(--theme-color-3);
+                top: ${infoActiveArrowTop};
+                left: ${infoActiveArrowLeft};
                 transition: 0s;
             }
 
             #output>button::after{
                 content: "${infoText}";
+                display: flex;
+                justify-content: center;
+                align-items: center;
                 opacity: 0;
                 position: absolute;
-                top: 0;
-                left: 50%;
-                width: ${infoWidth}px;
-                height: ${infoHeight}px;
-                transition: 0s 1s;
+                left: ${infoLeft};
+                top: ${infoTop};
+                width: 0;
+                height: 0;
+                transform: ${infoTransform};
+                transition: 0s ${infoTimespan}s;
                 text-align: center;
                 border-radius: 5px;
+                background-color: ${infoColor};
+                padding: 0;
+                z-index: 1;
+                color: ${infoFontColor};
             }
 
             #output>button:not([disabled]):active::after{
                 opacity: 1;
-                top: ${-(infoArrowLength + buttonBorderWidth)}px;
-                
-                transform: translate(-50%, -100%);
-                background-color: var(--theme-color-3);
-                padding: 5px;
+                top: ${infoActiveTop};
+                left: ${infoActiveLeft};
                 transition: 0s;
+                width: ${infoWidth}px;
+                height: ${infoHeight}px;
+                padding: 5px;
             }
 
             #output>button:disabled{
                 filter: grayscale(100%);
+            }
+
+            @media (hover: hover) {
+                #output>button:not([disabled]):hover{
+                    background-color: ${buttonHoverColor};
+                    cursor: pointer;
+                }
             }
         `;
     }else if(infoTriggerClicked){
@@ -144,56 +254,72 @@ update = () => {
                 content: "";
                 opacity: 0;
                 position: absolute;
-                top: 0;
-                width: ${infoArrowSize};
-                height: ${infoArrowLength};
-                transition: 0s 1s;
-
-                background-color: ${0};
-                -webkit-mask-image: ${0};
-                mask-image: ${0};
-                -webkit-mask-size: ${0}px;
-                mask-size: ${0}px;
+                top: ${infoTop};
+                left: ${infoLeft};
+                width: ${infoArrowWidth};
+                height: ${infoArrowHeight};
+                transform: ${infoTransform};
+                transition: 0s ${infoTimespan}s;
+                background-color: ${infoColor};
+                -webkit-mask-image: ${imgUrl};
+                mask-image: ${imgUrl};
+                -webkit-mask-size: ${infoArrowWidth} ${infoArrowHeight};
+                mask-size: ${infoArrowWidth} ${infoArrowHeight};
                 -webkit-mask-position: center center;
                 mask-position: center center;
                 -webkit-mask-repeat: no-repeat;
                 mask-repeat: no-repeat;
                 background-blend-mode: overlay;
+                z-index: 1;
             }
             
             #output>button:hover::before{
                 opacity: 1;
-                left: 50%;
-                transform: translate(-50%, -100%);
-                border-top: ${infoArrowLength + 1}px solid var(--theme-color-3);
+                top: ${infoActiveArrowTop};
+                left: ${infoActiveArrowLeft};
                 transition: 0s;
             }
 
             #output>button::after{
                 content: "${infoText}";
+                display: flex;
+                justify-content: center;
+                align-items: center;
                 opacity: 0;
                 position: absolute;
-                top: 0;
-                left: 50%;
-                width: ${infoWidth}px;
-                height: ${infoHeight}px;
-                transition: 0s 1s;
+                top: ${infoTop};
+                left: ${infoLeft};
+                width: 0;
+                height: 0;
+                transform: ${infoTransform};
+                transition: 0s ${infoTimespan}s;
                 text-align: center;
                 border-radius: 5px;
+                background-color: ${infoColor};
+                padding: 0;
+                z-index: 1;
+                color: ${infoFontColor};
             }
 
             #output>button:hover::after{
                 opacity: 1;
-                top: ${-(infoArrowLength + buttonBorderWidth)}px;
-                
-                transform: translate(-50%, -100%);
-                background-color: var(--theme-color-3);
-                padding: 5px;
+                top: ${infoActiveTop};
+                left: ${infoActiveLeft};
                 transition: 0s;
+                width: ${infoWidth}px;
+                height: ${infoHeight}px;
+                padding: 5px;
             }
 
             #output>button:disabled{
                 filter: grayscale(100%);
+            }
+
+            @media (hover: hover) {
+                #output>button:not([disabled]):hover{
+                    background-color: ${buttonHoverColor};
+                    cursor: pointer;
+                }
             }
         `;
     }
@@ -208,15 +334,17 @@ random = () => {
     buttonBorderColorInput.value = randColor();
     buttonFontColorInput.value = randColor();
     buttonHoverColorInput.value = randColor();
-    infoInput.value = randInt(infoInput.min, infoInput.max);
+    infoInput.checked = randBool();
     [infoTriggerHoverInput, infoTriggerClickedInput][randInt(0, 2)].checked = true;
-    infoLocationInput.value = randInt(infoLocationInput.min, infoLocationInput.max);
+    [infoLocationTopInput, infoLocationRightInput, infoLocationBottomInput, infoLocationLeftInput][randInt(0, 4)].checked = true;
     infoTextInput.value = "info text";
-    infoTimespanInput.value = randInt(infoTimespanInput.min, infoTimespanInput.max);
+    infoTimespanInput.value = randFloat(infoTimespanInput.min, infoTimespanInput.max);
     infoWidthInput.value = randInt(infoWidthInput.min, infoWidthInput.max);
     infoHeightInput.value = randInt(infoHeightInput.min, infoHeightInput.max);
     infoArrowLengthInput.value = randInt(infoArrowLengthInput.min, infoArrowLengthInput.max);
     infoArrowSizeInput.value = randInt(infoArrowSizeInput.min, infoArrowSizeInput.max);
+    infoColorInput.value = randColor();
+    infoFontColorInput.value = randColor();
 }
 
 clipboard = () => {
